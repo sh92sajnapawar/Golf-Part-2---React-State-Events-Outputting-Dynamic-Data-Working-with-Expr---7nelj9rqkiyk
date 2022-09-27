acd64a631b7bbec8598a2c60b759ba1af08c3114
sh92sajnapawar <108973943+sh92sajnapawar@users.noreply.github.com>
2022-09-27 16:07:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-[8:55 pm, 12/09/2022] +91 88816 18384: import React, { Component, useState, useEffect, useCallback } from "react";
+import React, { Component, useState, useEffect, useCallback } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -24,5 +24,61 @@ const App = () => {
         "px";
     } else if (event.keyCode === 39) {
       ballPositionClone.left =
-        Number(ballPosition.left.slice(0, ballP…
-[8:56 pm, 12/09/2022] +91 88816 18384: Ye golf part 2 ka hai issko app.js mai paste kardena
+        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) +
+        5 +
+        "px";
+    } else if (event.keyCode === 38) {
+      ballPositionClone.top =
+        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) -
+        5 +
+        "px";
+    } else if (event.keyCode === 40) {
+      ballPositionClone.top =
+        Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) +
+        5 +
+        "px";
+    }
+    // ballPositionClone.left = (Number(ballPosition.left.slice(0,ballPosition.left.indexOf("p")))+5)+"px";
+    setBallPosition(ballPositionClone);
+    // console.log(ballPosition);
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", keyHandler);
+    return () => window.removeEventListener("keydown", keyHandler);
+  });
+
+  const reset = () => {
+    setRenderBall(!renderBall);
+    setBallPosition({ left: "0px", top: "0px" });
+  };
+  const renderChoice = () => {};
+
+  let compo;
+  if (renderBall) {
+    compo = (
+      <div
+        className="ball"
+        style={{ left: ballPosition.left, top: ballPosition.top }}
+      ></div>
+    );
+  } else {
+    compo = (
+      <button onClick={start} className="start">
+        Start
+      </button>
+    );
+  }
+
+  return (
+    <div className="playground">
+      {compo}
+      <button onClick={reset} className="reset">
+        Reset
+      </button>
+      {renderChoice()}
+    </div>
+  );
+};
+
+export default App;
