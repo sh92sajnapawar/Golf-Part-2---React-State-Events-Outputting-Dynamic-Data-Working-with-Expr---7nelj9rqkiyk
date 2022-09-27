7c7ca67dd7df9c67b53d68c4c8c7479d0391a802
sh92sajnapawar <108973943+sh92sajnapawar@users.noreply.github.com>
2022-09-27 16:03:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,28 @@
-import React, { Component, useState } from "react";
+[8:55 pm, 12/09/2022] +91 88816 18384: import React, { Component, useState, useEffect, useCallback } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
 
-  return (
-    <div className="playground">
-      <button onClick={reset} className="reset">
-        Reset
-      </button>
-      {renderChoice()}
-    </div>
-  );
-};
+  const start = () => {
+    setRenderBall(!renderBall);
+  };
 
-export default App;
+  const keyHandler = (event) => {
+    let ballPositionClone = { ...ballPosition };
+
+    if (event.keyCode === 37) {
+      ballPositionClone.left =
+        Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) -
+        5 +
+        "px";
+    } else if (event.keyCode === 39) {
+      ballPositionClone.left =
+        Number(ballPosition.left.slice(0, ballP…
+[8:56 pm, 12/09/2022] +91 88816 18384: Ye golf part 2 ka hai issko app.js mai paste kardena
